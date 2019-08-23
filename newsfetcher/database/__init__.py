973fb93b22e02444526d9a159491f6983f89504b
René <luijk.r@gmail.com>
2019-08-23 12:39:05 +0200

    Add insert_items and get_from_table functions to databaseclient

--- newsfetcher/database/__init__.py
+++ newsfetcher/database/__init__.py
@@ -1,30 +1,34 @@
+import uuid
 from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster, NoHostAvailable
 from cassandra.query import SimpleStatement
 
 
 class DatabaseClient:
-    def __init__(self, host, keyspace, table):
+    def __init__(self, host, keyspace):
         self.host = host
         self.keyspace = keyspace
-        self.table = table
         try:
             self.client = Cluster([self.host]).connect(self.keyspace)
         except NoHostAvailable as e:
             print(e)
             self.client = Cluster()
 
-    # def insert_item(self, item):
-    #     query = SimpleStatement(
-    #         "INSERT INTO users (id, title, description, url, raw_xml, datetime_listed) VALUES (%s, %s, %s, %s, %s, %s)",
-    #         consistency_level=ConsistencyLevel.QUORUM)
-    #     try:
-    #         self.client.execute(query, (id, item.title, item.description, item.url, item.raw_xml, item.datetime_listed))
-    #     except:
-    #         print("Oh no!")
-    #
-    # def insert_items(self, items):
-    #     for item in items:
-    #         self.insert_item(item)
-    #     print("Inserted {} items".format(len(items)))
+    def get_from_table(self, table):
+        query = "SELECT * FROM {}".format(table)
+        statement = SimpleStatement(query, consistency_level=ConsistencyLevel.QUORUM)
+        return self.client.execute(statement)
 
+    def insert_item(self, item, table):
+        article_id = uuid.uuid3(uuid.NAMESPACE_URL, item.url)
+        query = "INSERT INTO {} (article_id, datetime_listed, source, title, description, url, raw_xml) VALUES (%s, %s, %s, %s, %s, %s, %s)".format(table)
+        statement = SimpleStatement(query, consistency_level=ConsistencyLevel.QUORUM)
+        try:
+            self.client.execute(statement, (article_id, item.datetime_listed, item.source, item.title, item.description, item.url, item.raw_xml))
+        except Exception as e:
+            print("Oh no!: {}".format(e))
+
+    def insert_items(self, items, table):
+        for item in items:
+            self.insert_item(item, table)
+        print("Inserted {} items".format(len(items)))
