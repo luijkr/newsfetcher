148d24b66e6d11429f4dc1103bffece57b7c675e
René <luijk.r@gmail.com>
2019-08-23 12:38:12 +0200

    Add tables to config

--- newsfetcher/config/__init__.py
+++ newsfetcher/config/__init__.py
@@ -3,10 +3,15 @@ class RssUrls:
         self.nyt = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
 
 
+class Tables:
+    def __init__(self):
+        self.raw = "article_list"
+        self.analyzed = ""
+
 class Config:
     def __init__(self):
         self.rss_urls = RssUrls()
         self.valid_sites = ["nyt"]
         self.host = "127.0.0.1"
         self.keyspace = "test01"
-        self.table = "test_table" # "article_list"
+        self.tables = Tables()
