6eeb5ae2cb8a860bc890e19a5a2f0eeca20fc8d6
René <luijk.r@gmail.com>
2019-08-23 20:17:50 +0200

    Replace url argument to call function

--- newsfetcher/__init__.py
+++ newsfetcher/__init__.py
@@ -7,23 +7,20 @@ from newsfetcher.config import Config
 conf = Config()
 
 
-class NewsClient:
-    def __init__(self, site):
+class NewsClient(object):
+    def call(self, site):
         if site not in conf.valid_sites:
             raise ValueError("Client: 'site' must be one of %r." % ", ".join(conf.valid_sites))
 
-        self.site = site
-        self.url = conf.rss_urls.__getattribute__(site)
-
-    def call(self):
+        url = conf.rss_urls.__getattribute__(site)
         now = datetime.utcnow()
         datetime_listed = now.strftime("%Y-%m-%d %H:%M:%S")
         version = now.strftime("%Y-%m-%d")
-        response = requests.get(self.url)
+        response = requests.get(url)
         if response.ok:
             content = response.content
             tree = Etree.ElementTree(Etree.fromstring(content))
             items = tree.find("channel").findall("item")
-            return [get_item_info(item, self.site, datetime_listed, version) for item in items]
+            return [get_item_info(item, site, datetime_listed, version) for item in items]
         else:
             return [Item("", "", "")]
