add8ec97d86ff1cbd8bc74347a898f4ce62ed149
René <luijk.r@gmail.com>
2019-08-24 10:03:17 +0200

    add main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from newsfetcher import conf, NewsClient
 from newsfetcher.database import DatabaseClient
 
 db = DatabaseClient(conf.host, conf.keyspace)
-nyt_news = NewsClient("nyt")
-bbc_news = NewsClient("bbc")
-nyt_items = nyt_news.call()
-bbc_items = bbc_news.call()
-db.insert_items(bbc_items, conf.tables.raw)
+news = NewsClient()
+for site in conf.valid_sites:
+    items = news.call(site)
+    db.insert_items(items, conf.tables.raw)
